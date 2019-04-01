025ac05df3923a52960614a3202687de5d02c7c4
Gyokan <gyokan.nezhdetov@gmail.com>
2019-04-01 23:30:48 +0300

    Fixed broken formatting

--- 06.Summary/1_Movie-Queries.sql
+++ 06.Summary/1_Movie-Queries.sql
@@ -21,31 +21,31 @@ GROUP BY starName;
 SELECT DISTINCT movie.studioName, movie.title, movie.year
 FROM movie
 WHERE movie.year = ANY(SELECT MAX(movieInner.year)
-	 			       FROM movie as movieInner
-	      		       GROUP BY movieInner.studioName)
+		       FROM movie as movieInner
+		       GROUP BY movieInner.studioName)
 ORDER BY movie.studioName DESC;
 
 SELECT movieStar.name
 FROM movieStar
 WHERE movieStar.gender = 'M'
   AND movieStar.birthdate = (SELECT MAX(movieStarInner.birthdate)
-							 FROM movieStar as movieStarInner
-							 WHERE movieStarInner.gender = 'M');
+			     FROM movieStar as movieStarInner
+			     WHERE movieStarInner.gender = 'M');
 
 SELECT movie.studioName, starsIn.starName, COUNT(movie.title) as num_movies
 FROM movie JOIN starsIn
   ON movie.title = starsIn.movieTitle
 GROUP BY starsIn.starName, movie.studioName
 HAVING COUNT(movie.title) >= ALL(SELECT COUNT(movie.title)
-								 FROM movie JOIN starsIn
-								   ON movie.title = starsIn.movieTitle
-								 GROUP BY starsIn.starName, movie.studioName);
+				 FROM movie JOIN starsIn
+				   ON movie.title = starsIn.movieTitle
+				 GROUP BY starsIn.starName, movie.studioName);
 
 SELECT movie.title, movie.year, COUNT(starsIn.starName) as num_actors
 FROM movie JOIN starsIn
   ON movie.title = starsIn.movieTitle
 GROUP BY movie.title, movie.year
 HAVING COUNT(starsIn.starName) >= ALL(SELECT COUNT(movie.title)
-								      FROM movie JOIN starsIn
-								        ON movie.title = starsIn.movieTitle
-								      GROUP BY starsIn.movieYear, movie.studioName);
+				      FROM movie JOIN starsIn
+					ON movie.title = starsIn.movieTitle
+				      GROUP BY starsIn.movieYear, movie.studioName);
